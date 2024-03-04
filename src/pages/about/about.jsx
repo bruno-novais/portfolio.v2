35648e75e2e44c6be405500eb1b750907be60d66
Bruno Novais <novais99bruno@gmail.com>
2024-03-04 00:21:54 -0300

    creating the about screen

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,20 +1,32 @@
 import "./about.css";
-import {Paper, Box} from '@mui/material';
-
+import { Paper, Box, Button } from "@mui/material";
 
 function About() {
-
-    return (
-        <>
-            <div className="page about" id="about">
-                <Box className="content_container">
-                    <Paper className="about_section"elevation={3} />
-                    <Paper className="photo_section"elevation={3} />
-                    <Paper className="skill_section"elevation={3} />
-                </Box>
+  return (
+    <>
+      <div className="page about" id="about">
+        <Box className="content_container">
+          <div className="about_section">
+            <div className="about_header">
+              <span>navbar</span>
             </div>
-        </>
-    );
+
+            <div className="about_content">
+              <div className="about_content_tilte">about</div>
+              <div className="about_content_text">text about</div>
+            </div>
+
+            <div className="about_actions">
+              <Button variant="contained">Teste</Button>
+            </div>
+          </div>
+
+          <Paper className="photo_section" elevation={3}></Paper>
+          <Paper className="skill_section" elevation={3}></Paper>
+        </Box>
+      </div>
+    </>
+  );
 }
 
 export default About;
